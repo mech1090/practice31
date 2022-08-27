ea8633accfbb9a5c41c17068fd7893a84c4a1f59
Shahzad <mech1090@gmail.com>
2022-08-27 09:12:49 -0600

    main page setup

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require('config')
+require('./db')
+
+
+const app = express()
+
+app.use(express())
+app.use(cors())
+app.use(express.json())
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+
+app.get('/',(req,res)=>{
+    res.send('Testing ok')
+})
+
+app.get('*',(req,res)=>{
+    res.send('BAd_request')
+})
+
+mongoose.connection.once('open',()=>{
+    app.listen(config.get('port'),()=>{
+        console.log(`Server is running on port ${config.get('port')}`)
+    })
+
+    console.log('DB CONNECTED')
+})
